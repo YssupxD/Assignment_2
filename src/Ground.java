f565bdb26a8ee4e2e09a93c4414002cc77d07148
YssupxD <YssupxD@users.noreply.github.com>
2020-10-07 19:00:22 +1300

    Game is ready to run, I have done the whole frame of the game, there are somethings need to be modified. 1. Add background music and sound effects, sound files are in Resources/Audio directory. 2. Change the score number using the sprite sheet, I found sliced ones and put them under Resources/Images directory. 3. Add keyboard listener(space key to control the bird) 4. make the restart button work(now we can click anywhere and the game would restart.)

--- src/Ground.java
+++ src/Ground.java
@@ -5,27 +5,17 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class Ground extends Game {
+public class Ground {
     int x = 0;
     int y = 500;
     BufferedImage img;
-    public static Image back_ground;
+
     public Ground() {
         try {
             img = ImageIO.read(new File("Resources/Images/ground.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void load_Bg(){
-        ImageIcon obj1 = new ImageIcon("Resources/bg.png");
-        back_ground = obj1.getImage();
-    }
-
-
-    static void do_Drawing(Graphics g) {
-        g.drawImage(back_ground, 0, 0, null);
     }
 
     public void step() {
