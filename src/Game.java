e29e4833cdd66a9099f4670c34e964dd2a6411eb
lynvu97 <lynvu97@gmail.com>
2020-10-08 23:24:16 +1300

    Points up

--- src/Game.java
+++ src/Game.java
@@ -23,10 +23,14 @@ public class Game extends JPanel implements ActionListener{
     public BufferedImage startImg = null;
     public BufferedImage overImg = null;
 
+    public Image[] _point =  new Image[10];;
+
    //public static AudioManagement am1 = new AudioManagement();
    // public static Clip clip_wing;
 
+    //System
     AudioManagement am = new AudioManagement();
+
     Bird bird;
     Column c1;
     Column c2;
@@ -35,7 +39,6 @@ public class Game extends JPanel implements ActionListener{
     public Game() throws IOException {
         state = START;
         score = 0;
-
 
         backgroundImg = ImageIO.read(new File("Resources/Images/bg.png"));
         startImg = ImageIO.read(new File("Resources/Images/start.png"));
@@ -114,10 +117,7 @@ public class Game extends JPanel implements ActionListener{
         //super.paint(g);
         g.drawImage(backgroundImg, 0, 0, null);
         //set font for score, need to be replace with the sprite sheet number
-        Font font = new Font(Font.SANS_SERIF, Font.BOLD, 40);
-        g.setFont(font);
-        g.setColor(Color.white);
-        g.drawString(score + "", 40, 60);
+        //try to draw score img
         //make the bird rotate
         Graphics2D g2d = (Graphics2D) g;
         //rotate anti-clock wise
@@ -130,6 +130,9 @@ public class Game extends JPanel implements ActionListener{
         g.drawImage(c2.img, c2.x - c2.width / 2, c2.y - c2.height / 2, null);
         //paint the ground
         g.drawImage(ground.img, ground.x, ground.y, null);
+        if(state == RUNNING) {
+            Score_display(g);
+        }
         //paint game state(start menu and over menu)
         if(state == START) {
             g.drawImage(startImg, 0 , 0, null);
@@ -201,7 +204,51 @@ public class Game extends JPanel implements ActionListener{
         }
     }
 
+    public void Score_display(Graphics g){
+        Image _final_point;
+        try {
+             //System.out.println("loading");
+            _point[0] = ImageIO.read(new File("Resources/Images/num" + 0 + ".png"));
+            _point[1] = ImageIO.read(new File("Resources/Images/num" + 1 + ".png"));
+            _point[2] = ImageIO.read(new File("Resources/Images/num" + 2 + ".png"));
+            _point[3] = ImageIO.read(new File("Resources/Images/num" + 3 + ".png"));
+            _point[4] = ImageIO.read(new File("Resources/Images/num" + 4 + ".png"));
+            _point[5] = ImageIO.read(new File("Resources/Images/num" + 5 + ".png"));
+            _point[6] = ImageIO.read(new File("Resources/Images/num" + 6 + ".png"));
+            _point[7] = ImageIO.read(new File("Resources/Images/num" + 7 + ".png"));
+            _point[8] = ImageIO.read(new File("Resources/Images/num" + 8 + ".png"));
+            _point[9] = ImageIO.read(new File("Resources/Images/num" + 9 + ".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //g.drawImage(_point[0], 100, 100, null);
+        if(score<10){
+            for(int j=0;j<10;j++) {
+                if (score == j) {
+                    g.drawImage(_point[j], 430/2, 100, null);
+                }
+            }
+        }
+        if(score>=10){
+            String _score = Integer.toString(score);
+            System.out.println("To string" + _score);
+            char[] temp = _score.toCharArray();
+            int s1 = Character.getNumericValue(temp[0]);
+            int s2 = Character.getNumericValue(temp[1]);
+            //System.out.println("a " + temp[0]+ " " +temp[1]);
+            for(int a=0;a<10;a++) {
+                if (s1 == a) {
+                    g.drawImage(_point[a], 430/2, 100, null);
+                }
+            }
+            for(int b=0;b<10;b++) {
+                if ( s2 == b) {
+                    g.drawImage(_point[b], 430/2+20, 100, null);
+                }
+            }
+        }
 
+    }
 
     public void action1(){
 
